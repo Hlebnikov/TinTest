dff6e68a34faf03ddf57d2102d9fbb4fcf4ba401
khlebnikov <khlebnikov@icerockdev.com>
2017-08-27 19:06:15 +0700

    Initial Commit

---
+++ TinkoffNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinkoffNews
+//
+//  Created by Aleksandr X on 27.08.17.
+//  Copyright © 2017 Khlebnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
